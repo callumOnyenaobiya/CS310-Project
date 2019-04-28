0c71868b6dac43b5d4a838ca4fda09ce30dd3856
callum <callum@callum-XPS-13-9370>
2019-04-28 13:52:22 +0100

    Added appropriate licensing.

--- src/lz77grammar/SignatureStore.java
+++ src/lz77grammar/SignatureStore.java
@@ -13,8 +13,23 @@ import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 
 /**
- * SignatureStore data structure used to convert sequences into unique signatures. Using algorithms discussed
- * In report associated with this project. All functions are discussed in report.
+  * SignatureStore data structure used to convert sequences into unique signatures. Using algorithms discussed
+  * In report associated with this project. All functions are discussed in report.
+  
+  *   Copyright 2019 Callum Onyenaobiya
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ 
  * @author Callum Onyenaobiya
  */
 @SuppressWarnings("unused") class SignatureStore implements Serializable {
